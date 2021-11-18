19006ef829a4e694e66106a02bf7ed91605d6902
zxy340 <zhang376@buffalo.edu>
2021-11-18 12:36:29 -0500

    some notes for test.py

---
+++ LSTM_SpeakIntent/test.py
@@ -0,0 +1,9 @@
+# this file is just used to test the correctness of the code
+import numpy as np
+import torch.nn as nn
+import torch
+from torch.utils.data import DataLoader
+from LSTM import simpleLSTM
+import data
+import os
+
