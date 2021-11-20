f5ea1547d501bc1c85ad36af7047aba693a7094d
zxy340 <zhang376@buffalo.edu>
2021-11-20 15:04:23 -0500

    ML.py debug

--- LSTM_SpeakIntent/ML.py
+++ LSTM_SpeakIntent/ML.py
@@ -4,6 +4,7 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import cross_val_score, KFold, cross_val_predict
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import os
 
 Concepts = [
     'Inner_Brow_Raiser',     # AU01
@@ -49,7 +50,7 @@ users = [
     'phoung',             # 20
     'Tracy_chen'          # 21
 ]
-label_index = 0  # indicate which concept to train the model
+label_index = 7  # indicate which concept to train the model
 data_path = '/mnt/stuff/xiaoyu/data/'  # the path where 'x_data.npy' and 'y_data.npy' are located
 model_type = 'LSTM/'
 
@@ -64,10 +65,16 @@ x_test = np.zeros((1, 128, 192))
 y_test = np.zeros((1,))
 for i in range(int(len(users)/16*9)):
     user = users[i]
+    print('Current added user is {}'.format(user))
+    if not os.path.exists(data_path + user + '/' + Concepts[label_index] + '/x_data.npy'):
+        continue
     x_train = np.concatenate((x_train, np.load(data_path + user + '/' + Concepts[label_index] + '/x_data.npy')), axis=0)
     y_train = np.concatenate((y_train, np.load(data_path + user + '/' + Concepts[label_index] + '/y_data.npy')), axis=0)
 for i in range(int(len(users)/16*9), int(len(users)/4*3)):
     user = users[i]
+    print('Current added user is {}'.format(user))
+    if not os.path.exists(data_path + user + '/' + Concepts[label_index] + '/x_data.npy'):
+        continue
     x_test = np.concatenate((x_test, np.load(data_path + user + '/' + Concepts[label_index] + '/x_data.npy')), axis=0)
     y_test = np.concatenate((y_test, np.load(data_path + user + '/' + Concepts[label_index] + '/y_data.npy')), axis=0)
 x_train = x_train[1:]
@@ -80,12 +87,20 @@ print("the length of x_test is {}".format(np.shape(x_test)))
 print("the length of y_test is {}".format(np.shape(y_test)))
 seq = np.arange(0, len(x_train), 1)
 np.random.shuffle(seq)
-x_train = x_train[seq[:]]
-y_train = y_train[seq[:]]
+x_train = x_train[seq[:50000]]
+y_train = y_train[seq[:50000]]
 seq = np.arange(0, len(x_test), 1)
 np.random.shuffle(seq)
-x_test = x_test[seq[:]]
-y_test = y_test[seq[:]]
+x_test = x_test[seq[:10000]]
+y_test = y_test[seq[:10000]]
+print("the length of x_train is {}".format(np.shape(x_train)))
+print("the length of y_train is {}".format(np.shape(y_train)))
+print("the length of x_test is {}".format(np.shape(x_test)))
+print("the length of y_test is {}".format(np.shape(y_test)))
+x_train = np.reshape(x_train, (len(x_train), -1))
+y_train = np.reshape(y_train, (len(y_train), -1))
+x_test = np.reshape(x_test, (len(x_test), -1))
+y_test = np.reshape(y_test, (len(y_test), -1))
 print("the length of x_train is {}".format(np.shape(x_train)))
 print("the length of y_train is {}".format(np.shape(y_train)))
 print("the length of x_test is {}".format(np.shape(x_test)))
@@ -94,27 +109,27 @@ print("the length of y_test is {}".format(np.shape(y_test)))
 
 # ......................train and test random forest model.........................
 forest = RandomForestClassifier()
-forest.fit(x_train, y_train)
+forest.fit(x_train, y_train.ravel())
 y_pred = forest.predict(x_test)
-print('the final confusion_matrix outcome of RandomForest model is {}'.format(confusion_matrix(y_test, y_pred)))
-print('the classification report of the concept {} is {}'.format(Concepts[label_index], classification_report(y_test, y_pred)))
-print('the accuracy of the concept {} is {}'.format(Concepts[label_index], accuracy_score(y_test, y_pred)))
+print('the final confusion_matrix outcome of RandomForest model is {}'.format(confusion_matrix(y_test.ravel(), y_pred)))
+print('the classification report of the concept {} is {}'.format(Concepts[label_index], classification_report(y_test.ravel(), y_pred)))
+print('the accuracy of the concept {} is {}'.format(Concepts[label_index], accuracy_score(y_test.ravel(), y_pred)))
 # .................................................................................
 
 # ......................train and test SVM model...................................
 svclassifier = SVC(kernel='linear')
-svclassifier.fit(x_train, y_train)
+svclassifier.fit(x_train, y_train.ravel())
 y_pred = svclassifier.predict(x_test)
-print('the final confusion_matrix outcome of SVM model is {}'.format(confusion_matrix(y_test, y_pred)))
-print('the classification report of the concept {} is {}'.format(Concepts[label_index], classification_report(y_test, y_pred)))
-print('the accuracy of the concept {} is {}'.format(Concepts[label_index], accuracy_score(y_test, y_pred)))
+print('the final confusion_matrix outcome of SVM model is {}'.format(confusion_matrix(y_test.ravel(), y_pred)))
+print('the classification report of the concept {} is {}'.format(Concepts[label_index], classification_report(y_test.ravel(), y_pred)))
+print('the accuracy of the concept {} is {}'.format(Concepts[label_index], accuracy_score(y_test.ravel(), y_pred)))
 # .................................................................................
 
 # ......................train and test Naive Bayes model...........................
 Bayes = GaussianNB()
-Bayes.fit(x_train, y_train)
+Bayes.fit(x_train, y_train.ravel())
 y_pred = Bayes.predict(x_test)
-print('the final confusion_matrix outcome of NaiveBayes model is {}'.format(confusion_matrix(y_test, y_pred)))
-print('the classification report of the concept {} is {}'.format(Concepts[label_index], classification_report(y_test, y_pred)))
-print('the accuracy of the concept {} is {}'.format(Concepts[label_index], accuracy_score(y_test, y_pred)))
+print('the final confusion_matrix outcome of NaiveBayes model is {}'.format(confusion_matrix(y_test.ravel(), y_pred)))
+print('the classification report of the concept {} is {}'.format(Concepts[label_index], classification_report(y_test.ravel(), y_pred)))
+print('the accuracy of the concept {} is {}'.format(Concepts[label_index], accuracy_score(y_test.ravel(), y_pred)))
 # .................................................................................
